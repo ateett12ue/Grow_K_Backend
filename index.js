24b2679181373b6a693f948e984bb0d2d78c2b9f
ateet <16ucs050@lnmiit.ac.in>
2020-01-08 11:49:09 +0530

    added files

--- index.js
+++ index.js
@@ -9,15 +9,63 @@ const credentials = require("./credentials.json");
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: "./",
+  filename(req, file, cb) {
+    const newFilename = `${uuid()}-${file.originalname}`;
+    cb(null, newFilename);
+  }
+});
+
+const uploadVideoFile = multer({ storage }).single("videoFile");
+
 app.use(express.json());
 app.use(cors({ origin: 3000 }));
 
-// const oAuth = youtube.authenticate({
-//   type: "oauth",
-//   client_id: credentials.web.client_id,
-//   client_secret: credentials.web.client_secret,
-//   redirect_url: credentials.web.redirect_uris[0]
-// });
+const oAuth = youtube.authenticate({
+  type: "oauth",
+  client_id: credentials.web.client_id,
+  client_secret: credentials.web.client_secret,
+  redirect_url: credentials.web.redirect_uris
+});
+
+app.post("/upload", uploadVideoFile, (req, res) => {
+  if (req.file) {
+    const { title, description } = req.body;
+    oAuth.generateAuthUrl({
+      access_type: "offline",
+      scope: "http://www.googleapis.com/auth/video.upload",
+      state: JSON.stringify({ filename: req.file.filename, title, description })
+    });
+  }
+});
+
+app.get("/oauth2callback", (req, res) => {
+  res.redirect("http://localhost:3000/uploadVideo/Success");
+  const { filename, title, description } = JSON.parse(req.query.state);
+  oAuth.getTocken(req.query.code, (err, tockens) => {
+    if (err) {
+      return console.error(err);
+    }
+    oAuth.setCredentials(tockens);
+    youtube.videos.insert(
+      {
+        resource: {
+          snippet: { title, description },
+          status: { privacyStatus: "private" }
+        },
+        part: "snippet,status",
+        media: {
+          body: fs.createReadStream(filename)
+        }
+      },
+      (err, data) => {
+        console.log("Done.");
+        process.exit();
+      }
+    );
+  });
+});
 
 const PORT = 4000;
 
